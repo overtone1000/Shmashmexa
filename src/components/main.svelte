1640417283db032e075476f0025ed03863081904
Tyler <tylerrmoore@gmail.com>
2026-02-21 19:24:37 -0800

    Basic hello world

---
+++ src/components/main.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    function navigate (dest:string) {window.location.href=dest;}
+</script>
+
+<div class="main">
+    Hello world
+    <iframe class="full-width" src="http://10.10.10.10:8123/dashboard-cameras/0" title="Dashboard-Cameras">
+        <p>iframe unsupported</p>
+    </iframe>
+</div>
+
+<style>
+    .full-width
+    {
+        width:100%;
+        flex-grow: 1;
+    }
+    .main
+    {
+        width: 100vw;
+        height:100vh;
+        margin: 0px;
+        display:flex;
+        flex-direction: column;
+    }
+</style>
