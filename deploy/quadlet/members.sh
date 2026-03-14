7e35f2e7f1fc4a208c3f8c88fa9266d997f9cf2b
Tyler <tylerrmoore@gmail.com>
2026-03-14 10:51:23 -0700

    Building out nginx container

--- deploy/quadlet/members.sh
+++ deploy/quadlet/members.sh
@@ -4,12 +4,14 @@ set -e
 
 QUADLET_MEMBERS=(
     #Pod first
+    shmashmexa-pod
     #Resources next
     shmashmexa_config-volume
     #Container builds
     shmashmexa-build
     #Containers in dependency order
     shmashmexa
+    nginx
 )
 
 echo Quadlet members are: ${QUADLET_MEMBERS[@]}
